e3d6bba2e7061c43aa08a6b775be3160ffda7dee
Lianjfkt <febriandyjalian@gmail.com>
2026-07-10 08:58:47 +0700

    feat: install and integrate @vercel/speed-insights

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { useAuthStore } from './stores/authStore'
 import AppRouter from './routes/AppRouter'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 function App() {
   const fetchUser = useAuthStore((state) => state.fetchUser)
@@ -15,6 +16,7 @@ function App() {
     <BrowserRouter>
       <AppRouter />
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   )
 }
